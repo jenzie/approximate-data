044294cbbc729b20e33a56353dbaa36696bf7f40
Jenny Zhen <coffee@csh.rit.edu>
2013-06-03 19:40:35 -0400

    Started working on the interface for the strategy pattern.

--- src/strategy/ApproximationStrategy.java
+++ src/strategy/ApproximationStrategy.java
@@ -1,9 +1,20 @@
 package strategy;
 
+import composite.XMLComponent;
+
 /**
  * @author: Jenny Zhen; jenny.zhen@rit.edu date: 06.03.2013 language: Java
  * project: approximate-data
  */
 
 public class ApproximationStrategy {
+	private XMLComponent root;
+
+	public ApproximationStrategy(XMLComponent root) {
+		this.root = root;
+	}
+
+	private XMLComponent approximate() {
+		return root;
+	}
 }
