869ac6210f69a660a1c9aafa68493e68697f9342
Jenny Zhen <coffee@csh.rit.edu>
2013-06-04 11:49:16 -0400

    Added comments.

--- src/strategy/ApproximationStrategy.java
+++ src/strategy/ApproximationStrategy.java
@@ -9,9 +9,24 @@ import composite.XMLComponent;
  * project: approximate-data
  */
 
+/**
+ * ApproximationStrategy
+ * Interface for the strategy design pattern.
+ */
 public interface ApproximationStrategy {
+	/**
+	 * @return the number of variables changed using the strategy.
+	 */
 	public int getCount();
 
+	/**
+	 * Perform approximation based on the strategy's specific algorithm.
+	 * @param node current node to approximate.
+	 * @param find data type to find
+	 * @param replace data type to replace the original/find with
+	 * @return the root node of the modified tree, which is necessary for
+	 * 			comparing results of multiple strategies.
+	 */
 	public XMLComponent approximate(
 			XMLComponent node, String find, String replace);
 }
--- src/strategy/NaiveStrategy.java
+++ src/strategy/NaiveStrategy.java
@@ -9,32 +9,57 @@ import composite.XMLComponent;
  * project: approximate-data
  */
 
+/**
+ * NaiveStrategy
+ * Concrete strategy for strategy design pattern.
+ * Changes all variables of type 'find' to type 'replace'.
+ */
 public class NaiveStrategy implements ApproximationStrategy {
 	private int count;
 
 	@Override
+	/**
+	 * @return the number of variables changed using the strategy.
+	 */
 	public int getCount() {
 		return this.count;
 	}
 
 	@Override
+	/**
+	 * Performs the approximation based on the strategy to replace all
+	 * existing variables.
+	 *
+	 * @param node current node to approximate.
+	 * @param find data type to find
+	 * @param replace data type to replace the original/find with
+	 * @return the root node of the modified tree, which is necessary for
+	 * 			comparing results of multiple strategies.
+	 */
 	public XMLComponent approximate(
 			XMLComponent node, String find, String replace) {
-		find = find.toLowerCase();
-		replace = replace.toLowerCase();
 
+		// ignore case and whitespace
+		find = find.toLowerCase().trim();
+		replace = replace.toLowerCase().trim();
+
+		// reset the count at the beginning of approximation
 		if(node.parent == null)
 			this.count = 0;
 
+		// if the current node has replaceable text
 		if(node.getText() != null) {
 			if(node.getText().toLowerCase().equals(find)) {
 				node.setText(replace);
 				this.count++;
 			}
 		}
+
+		// check the current node's children
 		for(XMLComponent child : node.children)
 			this.approximate(child, find, replace);
 
+		// return the root at the end of the first (final) iteration
 		return node;
 	}
 }
