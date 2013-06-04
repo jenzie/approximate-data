e744b4886d1d7b096dfd478a7815898c79f1446a
Jenny Zhen <coffee@csh.rit.edu>
2013-06-04 09:52:37 -0400

    Worked on the naive strategy for strategy.

--- src/strategy/NaiveStrategy.java
+++ src/strategy/NaiveStrategy.java
@@ -10,5 +10,30 @@ import composite.XMLComponent;
  */
 
 public class NaiveStrategy implements ApproximationStrategy {
-	
+	private int count;
+	private XMLComponent root;
+
+	public int getCount() {
+		return this.count;
+	}
+
+	public XMLComponent approximate(
+			XMLComponent root, String find, String replace) {
+		this.root = root;
+		XMLComponent pointer = root;
+		find = find.toLowerCase();
+		replace = replace.toLowerCase();
+
+		if(pointer.parent == null)
+			this.count = 0;
+
+		if(pointer.getText().toLowerCase().equals(find)) {
+			pointer.setText(replace);
+			this.count++;
+		}
+		for(XMLComponent child : pointer.children)
+			this.approximate(child, find, replace);
+
+		return this.root;
+	}
 }
